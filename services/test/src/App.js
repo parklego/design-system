58640f6b20d776f9b33d04d9a5985156d64914d1
parklego <hbd9425@gmail.com>
2024-04-27 04:55:07 +0900

    typography && box foundation

--- services/test/src/App.js
+++ services/test/src/App.js
@@ -1,21 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import styled from "@emotion/styled";
+import { variables } from "@parklego/themes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* here design token!! */}
+        <p className="heading4xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Text>
+          orange variables token color :{" "}
+          {variables.colors.$static.light.orange[400]}
+        </Text>
         <a
-          className="App-link"
+          className="text4xl"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          {}
+          welecome design token
         </a>
       </header>
     </div>
@@ -23,3 +30,8 @@ function App() {
 }
 
 export default App;
+
+const Text = styled.p`
+  color: var(--orange-400);
+  font-size: var(--font-size-30);
+`;
