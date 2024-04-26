47b0a2657c2a59c68bcd9c61d846138bfb177410
parklego <hbd9425@gmail.com>
2024-04-27 04:58:16 +0900

    typography && box foundation

--- packages/themes/script/build-css-module.js
+++ packages/themes/script/build-css-module.js
@@ -14,6 +14,7 @@ const toCssCasting = (str) => {
 const generateThemeCssVariables = () => {
   const cssLightStr = [];
   const cssDarkStr = [];
+  const cssOtherStr = [];
 
   Object.entries(theme.variables).forEach(([key, value]) => {
     if (key === "colors") {
@@ -41,20 +42,57 @@ const generateThemeCssVariables = () => {
           });
         }
       });
+    } else {
+      Object.entries(value).forEach(([mainKey, mainValue]) => {
+        Object.entries(mainValue).forEach(([subKey, subValue]) => {
+          const cssVariable = `--${toCssCasting(mainKey)}-${toCssCasting(
+            subKey
+          )}: ${subValue};`;
+          //   console.log(cssVariable)
+          cssOtherStr.push(`${cssVariable}`);
+        });
+      });
     }
   });
 
-  return [cssLightStr, cssDarkStr];
+  return [cssLightStr, cssDarkStr, cssOtherStr];
+};
+
+const generateThemeCssClasses = () => {
+  const cssClasses = [];
+  Object.entries(theme.classes).forEach(([, mainValue]) => {
+    Object.entries(mainValue).forEach(([subKey, subValue]) => {
+      Object.entries(subValue).forEach(([key, value]) => {
+        const className = `${subKey}${key}`;
+
+        const styleString = Object.entries(value)
+          .map(([prop, val]) => `${toCssCasting(prop)}: ${val};\n`)
+          .join(" ");
+
+        const classWithStyle = `.${className} {\n ${styleString} }\n`;
+
+        cssClasses.push(classWithStyle);
+      });
+    });
+  });
+
+  return cssClasses;
 };
 
 // css 파일 생성 함수
 const generateTheme = () => {
-  const [cssLightStr, cssDarkStr] = generateThemeCssVariables();
+  const [cssLightStr, cssDarkStr, cssOtherStr] = generateThemeCssVariables();
+  const cssClasses = generateThemeCssClasses();
 
   const lightFormat = `:root {\n${cssLightStr.join("\n")} \n}`;
   const darkFormat = `:root .theme-dark {\n${cssDarkStr.join("\n")} \n}`;
+  const otherFormat = `:root {\n${cssOtherStr.join("\n")} \n}`;
+  const classFormat = `${cssClasses.join("\n")} \n`;
 
-  fs.writeFileSync("dist/theme.css", `${lightFormat} \n\n${darkFormat}`);
+  fs.writeFileSync(
+    "dist/theme.css",
+    `${lightFormat} \n\n${darkFormat} \n\n${otherFormat} \n\n${classFormat}`
+  );
 };
 
 generateTheme();
---
+++ packages/themes/src/variables/box.ts
@@ -0,0 +1,17 @@
+export const spacing = {
+  "1": "0.25rem",
+  "2": "0.5rem",
+  "3": "0.75rem",
+  "4": "1rem",
+  "5": "1.25rem",
+};
+
+export const radius = {
+  none: "0",
+  sm: "0.125rem",
+  md: "0.375rem",
+  lg: "0.5rem",
+  "2xl": "1rem",
+};
+
+export const shadows = {};
